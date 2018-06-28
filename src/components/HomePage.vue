e72d10ad7241d14b2fd77461bf87d1f814fd4c99
gearmobile <gearmobile@gmail.com>
2018-06-28 20:59:50 +0300

    add vue-socket

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,12 +1,29 @@
 <template>
-
+  <section>
+    <p v-if="isConnected">We're connected to the server</p>
+    <p>Message from server: "{{ socketMessage }}"</p>
+    <button @click="pingServer()" type="button" class="btn btn-info text-capitalize">ping server</button>
+  </section>
 </template>
 
 <script>
   export default {
     name: 'HomePage',
-    props: {
-      msg: String
+    data() {
+      return {
+        isConnected: false,
+        socketMessage: ''
+      }
+    },
+    methods: {
+      pingServer() {
+        this.$socket.emit('pingServer', 'Ping');
+      }
+    },
+    sockets: {
+      connect() { this.isConnected = true },
+      disconnect() { this.isConnected = false },
+      messageChannel(data) { this.socketMessage = data }
     }
   }
 </script>
