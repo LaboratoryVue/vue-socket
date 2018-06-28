56a768ff7a2aa0600ea58d7ac629399ddf3c335a
gearmobile <gearmobile@gmail.com>
2018-06-28 20:42:36 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container" id="app">
+    <div class="row">
+      <div class="col">
+        <HomePage msg="Welcome to Your Vue.js App" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import HomePage from './components/HomePage.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      HomePage
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'HomePage',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style scoped>
+  /**/
+</style>
