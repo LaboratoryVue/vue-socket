03e5c35899e63a5b1b016b39f802a03e7202146a
gearmobile <gearmobile@gmail.com>
2018-07-03 13:16:02 +0300

    use clean websocket

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col">
         <h2>websocket</h2>
+        <button @click="onSend()" class="btn btn-info">send message</button>
       </div>
     </div>
   </div>
@@ -11,7 +12,30 @@
 <script>
 
   export default {
-    name: 'app'
+    name: 'app',
+    data() {
+      return {
+        ws: null
+      }
+    },
+    methods: {
+      onSend() {
+        this.ws.send('тест сообщение');
+      }
+    },
+    created() {
+      this.ws = new WebSocket('wss://echo.websocket.org/');
+      this.ws.onopen = function() {
+        console.log(`ws подключенно`);
+      };
+      this.ws.onclose = function(event) {
+        console.log(`ws закрыто по причине ${event}`);
+      };
+      this.ws.onmessage = function(message) {
+        console.log(message);
+        // console.log(`Сообщение ${message}`);
+      };
+    }
   }
 </script>
 
@@ -20,5 +44,8 @@
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+  }
+  button {
+    cursor: pointer;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
-import socketio from 'socket.io-client';
-import VueSocketIO from 'vue-socket.io';
+// import socketio from 'socket.io-client';
+// import VueSocketIO from 'vue-socket.io';
 
-export const SocketInstance = socketio('wss://echo.websocket.org/');
+// export const SocketInstance = socketio('wss://echo.websocket.org/');
 
-Vue.use(VueSocketIO, SocketInstance);
+// Vue.use(VueSocketIO, SocketInstance);
 
 Vue.config.productionTip = false
 
