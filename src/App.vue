0cf0792edc45ea9e6e789d05fe50d26f863d0b9f
gearmobile <gearmobile@gmail.com>
2018-07-03 13:28:34 +0300

    websocket - success

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@
     <div class="row">
       <div class="col">
         <h2>websocket</h2>
-        <button @click="onSend()" class="btn btn-info">send message</button>
+        <div class="form-group">
+          <input type="text" name="message" v-model="message">
+        </div>
+        <div class="form-group">
+          <button @click="onSend()" class="btn btn-info">send message</button>
+        </div>
+        <div class="form-group">
+          <p class="text-success">{{ answer }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -15,25 +23,24 @@
     name: 'app',
     data() {
       return {
+        message: '',
+        answer: '',
         ws: null
       }
     },
     methods: {
       onSend() {
-        this.ws.send('тест сообщение');
+        this.ws.send(this.message);
       }
     },
     created() {
       this.ws = new WebSocket('wss://echo.websocket.org/');
-      this.ws.onopen = function() {
-        console.log(`ws подключенно`);
-      };
-      this.ws.onclose = function(event) {
-        console.log(`ws закрыто по причине ${event}`);
-      };
-      this.ws.onmessage = function(message) {
-        console.log(message);
-        // console.log(`Сообщение ${message}`);
+      this.ws.onopen = () => console.log(`ws подключенно`);
+      this.ws.onclose = event => console.log(`ws закрыто по причине ${event}`);
+      this.ws.onerror = event => console.log(event);
+      this.ws.onmessage = message => {
+        this.answer = message.data;
+        this.message = '';
       };
     }
   }
